16656db5b46845fa5961d89b70c5d97c9e3ef92f
Abilka <51174445+Abilka@users.noreply.github.com>
2024-10-26 23:47:31 +0300

    убрано лишние

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,35 +1,26 @@
-import random
 import unittest
-from unittest import mock
 
 import faker
 import requests
 
+
 class TestRouter(unittest.TestCase):
     def setUp(self):
         self.client = requests.session()
-        headers = {
-            'accept': 'application/json',
-            'Content-Type': 'application/json',
-        }
+        headers = {'accept': 'application/json', 'Content-Type': 'application/json', }
         self.client.headers.update(headers)
-        self.user = {
-            'username': 'test',
-            'password': 'test',
-        }
+        self.user = {'username': 'test', 'password': 'test', }
         self.fake = faker.Faker()
 
-
-    @mock.patch('jwt.decode')
-    def test_refresh_token(self, mock_decode):
+    def test_refresh_token(self):
         response = self.client.post("http://localhost:5000/user/auth/login", json=self.user)
         refresh = response.json()['refresh_token']
         response = self.client.post("http://localhost:5000/user/auth/refresh", params={"refresh": refresh})
         self.assertEqual(response.status_code, 200)
 
     def test_register_user(self):
-        response = self.client.post("http://localhost:5000/user/auth/register", json={"username": self.fake.sentence(),
-                                                                                      "password":"wrong"})
+        response = self.client.post("http://localhost:5000/user/auth/register",
+                                    json={"username": self.fake.sentence(), "password": "wrong"})
         self.assertEqual(response.status_code, 200)
 
     def test_register_user_with_existing_username(self):
@@ -41,5 +32,6 @@ class TestRouter(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_login_with_invalid_credentials(self):
-        response = self.client.post("http://localhost:5000/user/auth/login", json={"username":"wrong", "password":"wrong"})
+        response = self.client.post("http://localhost:5000/user/auth/login",
+                                    json={"username": "wrong", "password": "wrong"})
         self.assertEqual(response.status_code, 400)
