4eaca5c20e10561005d7bb3801174e6763cbc92c
Abilka <51174445+Abilka@users.noreply.github.com>
2024-10-27 01:37:34 +0300

    добавлена проверка на юзера

--- api/user/operations/__init__.py
+++ api/user/operations/__init__.py
@@ -10,7 +10,7 @@ import database
 @route.get("/all", response_model=typing.List[models.OperationResponse])
 def get_operations(user: dict = fastapi.Depends(utils.get_current_user)):
     db: Session = database.Database().get_marker()
-    operations = db.query(Operation).filter_by(user=user["id"]).all()
+    operations = db.query(Operation).filter_by(user_id=user["id"]).all()
     data = []
     for operation in operations:
         data.append(operation)
