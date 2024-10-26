4a5bf267fa6606c94b660849056206d06f498c27
Abilka <51174445+Abilka@users.noreply.github.com>
2024-10-26 04:16:45 +0300

    метод для получения патрулей и решения их

--- api/user/patrol/__init__.py
+++ api/user/patrol/__init__.py
@@ -1,7 +1,31 @@
+import json
+
 import fastapi
+
+import database
 from .router import route
 from .. import utils
+from database.patrol import Patrol
 
 @route.get("/me")
-def get_achievements(current_user: str = fastapi.Depends(utils.get_current_user)):
-    return {"username": current_user}
+def get_achievements(user: dict = fastapi.Depends(utils.get_current_user)):
+    session = database.Database().get_marker()
+    patrols_user: list[database.patrol.Patrol] = session.query(Patrol).filter(
+        Patrol.user_id == user['id'] and Patrol.answers == ''
+    ).all()
+    patrols_user[0].__repr__()
+    return list(map(lambda x: x.__dict__, patrols_user))
+
+
+@route.post("/{id_patrol}")
+def answer(answers: str, id_patrol: int, user: dict = fastapi.Depends(utils.get_current_user)):
+    session = database.Database().get_marker()
+    patrols_mission = session.query(Patrol).filter_by(id=id_patrol)
+    patrols_mission.update({'answers': answers})
+
+    session.commit()
+    patrols_user: list[database.patrol.Patrol] = session.query(Patrol).filter(
+        Patrol.user_id == user['id'] and Patrol.answers == ''
+    ).all()
+    patrols_user[0].__repr__()
+    return patrols_mission.first()
