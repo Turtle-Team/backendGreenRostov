c14e263d9651f9a9d57b2c3dd4bdcec2a2aeac08
Abilka <51174445+Abilka@users.noreply.github.com>
2024-10-26 04:16:23 +0300

    патруль для всех юзеров

--- scheduler/patrol.py
+++ scheduler/patrol.py
@@ -6,21 +6,20 @@ from database import Database
 from database.user import User
 
 def create_patrol():
-    patrol = database.patrol.Patrol()
-    patrol.headers = 'Задача патруля: Ростов-на-Дону'
-
-    latitude = random.uniform(47.266681, 47.243828)
-    longitude = random.uniform(39.580366, 39.849199)
-
-    patrol.x_pos = latitude
-    patrol.y_pos = longitude
-    patrol.radius = 5
-
     session = Database().get_marker()
-    db_user = session.query(User).order_by(User.id).first()
-    patrol.user_id = db_user.id
+    for user in session.query(User).order_by(User.id).all():
+        patrol = database.patrol.Patrol()
+        patrol.headers = 'Задача патруля: Ростов-на-Дону'
 
-    session = Database().get_marker()
-    session.add(patrol)
-    session.commit()
+        latitude = random.uniform(47.266681, 47.243828)
+        longitude = random.uniform(39.580366, 39.849199)
+
+        patrol.x_pos = latitude
+        patrol.y_pos = longitude
+        patrol.radius = 5
+
+        patrol.user_id = user.id
+
+        session.add(patrol)
+        session.commit()
 
