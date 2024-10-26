b0e10da1d72505322dd789e965e3e0bdb0ba64b8
Abilka <51174445+Abilka@users.noreply.github.com>
2024-10-27 01:37:19 +0300

    убрана модель

--- api/user/operations/__init__.py
+++ api/user/operations/__init__.py
@@ -8,18 +8,10 @@ from database.operations import Operation
 import database
 
 @route.get("/all", response_model=typing.List[models.OperationResponse])
-def get_operations(user: dict = fastapi.Depends(utils.get_current_user)) -> typing.List[models.OperationResponse]:
+def get_operations(user: dict = fastapi.Depends(utils.get_current_user)):
     db: Session = database.Database().get_marker()
     operations = db.query(Operation).filter_by(user=user["id"]).all()
     data = []
     for operation in operations:
-        op = models.OperationResponse(
-            id=operation.id,
-            retailPlace=operation.retailPlace,
-            ecashTotalSum=operation.ecashTotalSum,
-            totalSum=operation.totalSum,
-            date_time=operation.date_time,
-            user=operation.user
-        )
-        data.append(op)
+        data.append(operation)
     return data
