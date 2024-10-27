21065e020432aa4d0f4fc29cd65f3e42a6e117b4
Abilka <51174445+Abilka@users.noreply.github.com>
2024-10-27 03:48:44 +0300

    получение своих мп

--- api/events/__init__.py
+++ api/events/__init__.py
@@ -3,13 +3,14 @@ import typing
 import fastapi
 from fastapi import HTTPException
 from sqlalchemy.orm import Session
-from . import models
-from api.user import utils
-from .route import route
-from database.events import Event, UserEvent
-from api.events.models import EventsResponse
-from database.user import User
+
 import database
+from api.events.models import EventsResponse
+from api.user import utils
+from database.events import Event, UserEvent
+from database.user import User
+from . import models
+from .route import route
 
 
 # Роутинг для событий
@@ -35,10 +36,8 @@ def register_user_to_event(registration: models.RegisterEvent, user: dict = fast
     if not user_exists:
         raise HTTPException(status_code=400, detail="User does not exist.")
 
-    existing_registration = db.query(UserEvent).filter(
-        UserEvent.user_id == user['id'],
-        UserEvent.event_id == registration.event_id
-    ).first()
+    existing_registration = db.query(UserEvent).filter(UserEvent.user_id == user['id'],
+                                                       UserEvent.event_id == registration.event_id).first()
 
     if existing_registration:
         raise HTTPException(status_code=400, detail="User is already registered for this event.")
@@ -60,9 +59,22 @@ def get_event_members(event_id: int, user: dict = fastapi.Depends(utils.get_curr
 
     members = db.query(User).join(UserEvent).filter(UserEvent.event_id == event_id).all()
     # member_list = [models.Member(first_name=member.first_name, last_name=member.last_name, patronymic=member.patronymic) for member in members]
-    member_list = [models.Member(first_name=member.username, last_name=member.username, patronymic=member.username) for member in members]
+    member_list = [models.Member(first_name=member.username, last_name=member.username, patronymic=member.username) for
+                   member in members]
 
     return models.EventMembersResponse(event_id=event_id, members=member_list)
+
+
+@route.get("/my")
+def get_event_members(user: dict = fastapi.Depends(utils.get_current_user)):
+    db: Session = database.Database().get_marker()
+    event = db.query(UserEvent).filter(UserEvent.user_id == user['id']).all()
+    if not event:
+        raise HTTPException(status_code=404, detail="Event not found")
+    events_ids = list(map(lambda x: x.event_id, event))
+    events = db.query(Event).filter(Event.id.in_(events_ids)).all()
+    return events
+
 
 @route.get("/all")
 def get_events(user: dict = fastapi.Depends(utils.get_current_user)) -> typing.List[EventsResponse]:
@@ -70,11 +82,7 @@ def get_events(user: dict = fastapi.Depends(utils.get_current_user)) -> typing.L
     events = db.query(Event).all()
     data = []
     for event in events:
-        e = EventsResponse(id=event.id,
-                           name=event.name,
-                           description=event.description,
-                           latitude=event.latitude,
-                           longitude=event.longitude,
-                           picture=event.picture)
+        e = EventsResponse(id=event.id, name=event.name, description=event.description, latitude=event.latitude,
+                           longitude=event.longitude, picture=event.picture)
         data.append(e)
     return data
