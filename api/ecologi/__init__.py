80d6a2c1cc95fc3227de8029d5f8bed842c58a6d
Abilka <51174445+Abilka@users.noreply.github.com>
2024-10-27 05:15:51 +0300

    добавлена модель для вывода

--- api/ecologi/__init__.py
+++ api/ecologi/__init__.py
@@ -2,14 +2,14 @@ import requests
 
 import setting
 from .route import route
-
+from . import models
 
 # Роутинг для событий
 @route.post("/uv")
-def create_event():
+def create_event() -> models.UvData:
     access_key = setting.YASHA_POGODA_KEY
 
     headers = {'X-Yandex-Weather-Key': access_key}
     response = requests.get('https://api.weather.yandex.ru/v2/forecast?lat=47.222110&lon=39.718808',
                             headers=headers).json()
-    return response['fact']['uv_index']
+    return models.UvData(uv=response['fact']['uv_index'])
--- scheduler/__init__.py
+++ scheduler/__init__.py
@@ -2,12 +2,14 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 import time
 from . import patrol
+from . import eco_rating
 
 
 def main():
     patrol.create_patrol()
     scheduler = BackgroundScheduler()
-    scheduler.add_job(patrol.create_patrol, 'interval', minutes=30)
+    scheduler.add_job(patrol.create_patrol, 'interval', hours=6)
+    scheduler.add_job(eco_rating.create, 'interval', miniutes=1)
     scheduler.start()
 
     try:
